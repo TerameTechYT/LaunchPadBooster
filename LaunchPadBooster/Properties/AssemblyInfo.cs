26c86d4e1d3bae92d5ad7583073b344cb32aa480
vivi <terametech@outlook.com>
2025-11-06 10:54:10 -0500

    add assembly info

---
+++ LaunchPadBooster/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using static LaunchPadBooster.LaunchPadBooster;
+using System.Reflection;
+
+[assembly: AssemblyTitle(NAME)]
+[assembly: AssemblyDescription(NAME)]
+[assembly: AssemblyCompany(NAME)]
+[assembly: AssemblyProduct(NAME)]
+[assembly: AssemblyVersion(VERSION)]
+[assembly: AssemblyFileVersion(VERSION)]
