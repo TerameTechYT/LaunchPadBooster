cbe8551c7fd1de4642b706a6fb4ca493fc7551df
vivi <terametech@outlook.com>
2025-11-07 17:33:26 -0800

    Updated build system (#27)

--- LaunchPadBooster/LaunchPadBooster.cs
+++ LaunchPadBooster/LaunchPadBooster.cs
@@ -2,6 +2,8 @@ namespace LaunchPadBooster
 {
   public static class LaunchPadBooster
   {
+    public const string NAME = "LaunchPadBooster";
+    public const string GUID = "launchpad.booster";
     public const string VERSION = "0.1.6";
   }
 }
